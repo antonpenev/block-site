330bd867792211548c2ccad92124e6a7bb796ae6
Anton Penev <antonpenev@gmail.com>
2022-09-01 15:27:28 +0300

    Changing some color and url formating

--- blocked.js
+++ blocked.js
@@ -1,4 +1,5 @@
 /* global window, document, URLSearchParams */
 
 const url = (new URLSearchParams(window.location.search)).get("url");
-document.getElementById("url").textContent = url;
+document.getElementById("url").innerHTML =
+  '<a href=" ' + url + '">' + url + "</a>";
